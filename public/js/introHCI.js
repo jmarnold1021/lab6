5557ed3ae97e247fc07127626a893451eecd5efd
vagrant <vagrant@precise32.(none)>
2015-02-12 17:55:44 +0000

    ajax lab 6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,6 +14,30 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
+function getCallback(result){
+
+  console.log(result);
+	var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>'+result['summary'];
+
+  $('#project'+result['id'] + ' div').html(projectHTML);
+		 
+		
+}
+function randCallback(result){
+  console.log(result);
+	var colors = result['colors']['hex'];
+	$('body').css('background-color', colors[0]);
+  $('.thumbnail').css('background-color', colors[1]);
+  $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+  $('p').css('color', colors[3]);
+  $('.project img').css('opacity', .75);
+
+}
+
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
@@ -27,6 +51,13 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	console.log("retreiving project/" + idNumber);
+	
+	$.get("project/"+idNumber,getCallback);
+  
+	
+	
+
 }
 
 /*
@@ -35,4 +66,7 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+		$.get("palette/",randCallback);
+
+
 }
